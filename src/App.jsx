e59ed77015d0a541946ae7acf6881f780b20390e
JunaidLatif4 <junaidlatif004@gmail.com>
2024-08-27 23:50:55 +0500

    ui modifications

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
 import Wallet from "./components/Wallet"
+import Profile from "./components/Profile"
+import Buy from "./components/Buy"
+import Send from "./components/Send"
 
 
 
@@ -14,6 +17,9 @@ function App() {
       <Routes>
         <Route path="" element={<Wallet />} />
         <Route path="login" element={<Login />} />
+        <Route path='profile' element={<Profile/>} />
+        <Route path='buy' element={<Buy/>} />
+        <Route path='send' element={<Send/>} />
       </Routes>
     </>
   )
